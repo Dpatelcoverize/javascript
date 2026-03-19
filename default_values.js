68f650504a060b5511064b79201dee1e6d5068df
devanshi.p <devanshi.p@covrize.com>
2026-03-19 16:38:31 +0530

    array_destructuring

---
+++ default_values.js
@@ -0,0 +1,6 @@
+let arr = ["Devanshi"];
+
+let [name, age = 18] = arr;
+
+console.log(name);
+console.log(age);
---
+++ skip_values.js
@@ -0,0 +1,6 @@
+let arr = ["Devanshi", 20, "Ahmedabad"];
+
+let [name, , city] = arr;
+
+console.log(name);
+console.log(city);
